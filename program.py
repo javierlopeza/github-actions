547f2c22d378448f9fcb446477d8839af4dc3957
Javier López <javierlopez@uc.cl>
2020-05-06 02:24:02 -0400

    feat: fix string to write

--- program.py
+++ program.py
@@ -3,4 +3,4 @@ import pendulum
 today = pendulum.now("America/Santiago").format("YYYY-MM-DD HH:mm:ss")
 
 with open("./final.txt", "a") as f:
-    f.write("--- {} ---\n".format(today))
+    f.write("123".format("123"))
